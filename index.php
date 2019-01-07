edd2bac9c1b9b0e7eb90d1caf73d5aa6fa06a83d
Chockehaa <imtdkso@gmail.com>
2019-01-07 10:05:03 +0100

    Debut CMS

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php
+
+class CMS
+{
+    var $host;
+    var $username;
+    var $password;
+    var $table;
+
+
+public function display_public()
+{
+
+
+}
+
+public function display_admin()
+{
+
+
+}
+
+public function write()
+{
+
+
+
+}
+
+public function connect()
+{
+
+
+
+}
+
+public function buildDB()
+{
+
+
+
+}
+
+
+}
+?>
